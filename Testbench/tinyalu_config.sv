db64566a81b85d211f72e81b6f952b4ddd0a19fe
EmanYasser <emanyaser811@gmail.com>
2025-12-11 21:32:41 +0200

    Create tinyalu_config.sv

---
+++ Testbench/tinyalu_config.sv
@@ -0,0 +1,11 @@
+package tinyalu_config_pkg;
+    class tinyalu_config extends uvm_object;
+        `uvm_object_utils (tinyalu_config)
+
+         virtual tinyalu_if tinyalu_vif;
+         
+         function new (string name ="tinyalu_config");
+          super.new(name);
+         endfunction
+    endclass
+endpackage
