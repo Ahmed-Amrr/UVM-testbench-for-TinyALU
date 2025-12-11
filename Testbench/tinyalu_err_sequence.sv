a3158ebe39df0fb59d194fc9f48be557390c9e34
Aya Radwan <144179747+Aya-Radwan@users.noreply.github.com>
2025-12-11 19:40:05 +0200

    error sequence

---
+++ Testbench/tinyalu_err_sequence.sv
@@ -0,0 +1,20 @@
+class tinyalu_err_sequence extends tinyalu_main_seq;
+`uvm_object_utils(tinyalu_err_sequence)
+
+    tinyalu_seq_item e_item;
+
+    function new(string name = "tinyalu_err_sequence");
+        super.new(name);
+    endfunction //new()
+
+    virtual task body();
+        e_item = tinyalu_err_seq_item::type_id::create("e_item")
+        for (int i = 0; i < 100; i++) begin
+            start_item(e_item);
+            assert(e_item.randomize());
+            finish_item(e_item);
+        end
+    endtask: body
+endclass //tinyalu_err_sequence extends uvm_sequence
+
+
